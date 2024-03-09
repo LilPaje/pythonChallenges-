067dac322fbf909a9132f5643f06e4f7ee80a883
LilPaje <brunovinicius03@hotmail.com>
2024-03-09 03:00:45 -0300

    algumas implementações a mais

--- file_manager/main.py
+++ file_manager/main.py
@@ -23,12 +23,23 @@ def welcome():
 #    print(users)
 
 def remove_files(user, folder):
-    filepath = f'C:/Users/{user}/{folder}/teste'
+    filepath = f'C:/Users/{user}/{folder}'
     dir_file_list = os.listdir(filepath)
-    print(dir_file_list)
     for i in dir_file_list:
-        print(i)
-        os.remove(filepath+f"/{i}")
+        print(i, end=" ")
+
+    print("\nThe files above will be permanently excluded. Do you want to proceed?Y/n")
+    input_user = str(input("->"))
+    if input_user == 'Y' or 'y':
+        for i in dir_file_list:
+            print(i)
+            os.remove(filepath+f"/{i}")
+    elif input_user == 'N' or 'n':
+        print("Canceling...")
+        time.sleep(2.5)
+    else:
+        print("Invalid Number, please try again")
+        remove_files(user,folder)
 
 def windows_remove_files_from_folder(user):
     list_folders = ["Documents", "Images", "Downloads"]
@@ -40,20 +51,12 @@ def windows_remove_files_from_folder(user):
 
     input_user = str(input("-> "))
     if input_user == "0":
-        time.sleep(2.5)
         print("Canceling...")
-        main()
-
     elif input_user == "1" or input_user == "2" or input_user == "3":
         print("Removing...")
         remove_files(user,list_folders[int(input_user)-1])
-        time.sleep(2.5)
-        main()
-
     else:
         print("Invalid value, please try again")
-        time.sleep(2.5)
-        main()
     
 
 def main():
@@ -62,8 +65,24 @@ def main():
     print("[1] Clear folder")
     print("[2] Change Files Location")
     print("[0] Exit")
-    if platform.system() == 'Windows':
-        windows_remove_files_from_folder(os.getlogin())
+
+    input_user = str(input("-> "))
+    if input_user == "0":
+        print("Exiting the code")
+
+    elif input_user == '1':
+        if platform.system() == 'Windows':
+            windows_remove_files_from_folder(os.getlogin())
+            time.sleep(2.5)
+            main()
+    elif input_user == '2':
+        pass
+
+    else:
+        print("Invalid value, please try again")
+        time.sleep(2.5)
+        main()
+
 
 main()
 
