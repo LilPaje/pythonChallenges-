5ffef3503191ba9a8c2395c7af4df3232a955011
LilPaje <brunovinicius03@hotmail.com>
2024-03-18 15:09:37 -0300

    Final Implementation, It's Working, I should just change the directories and create a read.me, but i'll do this later :D

--- file_manager/main.py
+++ file_manager/main.py
@@ -23,32 +23,36 @@ def welcome():
 #    users = Path('./users.txt').read_text()
 #    print(users)
 
-def remove_files(user, folder):
-    filepath = f'C:/Users/{user}/{folder}'
-    dir_file_list = os.listdir(filepath)
-    for i in dir_file_list:
-        print(i, end=", ")
-
-    print("\nThe files above will be permanently excluded. Do you want to proceed?Y/n")
-    input_user = str(input("->"))
-    if input_user == 'Y' or 'y':
-        for i in dir_file_list:
-            #print(filepath+f"/{i}")
-            if os.path.isfile(filepath+f"/{i}"):
-                os.remove(filepath+f"/{i}")
-                #print(os.path.isdir(filepath+f"/{i}"))
-            else: 
-                shutil.rmtree(filepath+f"/{i}")
-
-    elif input_user == 'N' or 'n':
-        print("Canceling...")
-        time.sleep(2.5)
+def remove_files(user):
+    folder = choose_folder()
+    if folder == '-1':
+        pass
     else:
-        print("Invalid Number, please try again")
-        remove_files(user,folder)
+        filepath = f'C:/Users/{user}/{folder}'
+        dir_file_list = os.listdir(filepath)
+        for i in dir_file_list:
+            print(i, end=", ")
+
+        print("\nThe files above will be permanently excluded. Do you want to proceed?Y/n")
+        input_user = str(input("->"))
+        if input_user == 'Y' or 'y':
+            for i in dir_file_list:
+                #print(filepath+f"/{i}")
+                if os.path.isfile(filepath+f"/{i}"):
+                    os.remove(filepath+f"/{i}")
+                    #print(os.path.isdir(filepath+f"/{i}"))
+                else: 
+                    shutil.rmtree(filepath+f"/{i}")
+
+        elif input_user == 'N' or 'n':
+            print("Canceling...")
+            time.sleep(2.5)
+        else:
+            print("Invalid Number, please try again")
+            remove_files(user,folder)
 
 
-def windows_remove_files_from_folder(user):
+def choose_folder():
     list_folders = ["Documents", "Images", "Downloads/teste"]
     print("Which folder do you want to exclude the files?")
     print("[1] Documents")
@@ -58,17 +62,36 @@ def windows_remove_files_from_folder(user):
 
     input_user = str(input("-> "))
     if input_user == "0":
-        print("Canceling...")
+        print("Canceling")
+        return "-1"
     elif input_user == "1" or input_user == "2" or input_user == "3":
-        print("Removing...")
-        remove_files(user,list_folders[int(input_user)-1])
+        return list_folders[int(input_user)-1]
+    
     else:
         print("Invalid value, please try again")
+        choose_folder()
+    
 
+def organize_files(user):
+    #list_extensions = [".pdf",".exe",".msi",".jpg",".png",".jif",".txt"]
+    
 
-def organize_files(path):
-    list_extensions = [".pdf",".exe",".msi",".jpg",".png",".jif"]
-    list_itens_on_folder
+    input_user = choose_folder()
+    path = f'C:/Users/{user}/{input_user}'
+    list_itens_on_folder = os.listdir(path)
+    list_itens_destiny_folder = os.listdir(f'C:/Users/{user}/Documents/Izzy/teste')
+    if input_user == "-1":
+        pass
+    else: 
+        print("Organizing")
+        for j in list_itens_on_folder:
+            if j[-3:] in list_itens_destiny_folder:
+                shutil.move(path+'/'+j,f'C:/Users/{user}/Documents/Izzy/teste/'+j[-3:])
+                #print(j[-4:])
+            else:
+                shutil.move(path+'/'+j,f'C:/Users/{user}/Documents/Izzy/teste/other')
+            
+    
 
 def main():
     welcome() 
@@ -83,11 +106,12 @@ def main():
 
     elif input_user == '1':
         if platform.system() == 'Windows':
-            windows_remove_files_from_folder(os.getlogin())
+            remove_files(os.getlogin())
             time.sleep(2.5)
             main()
     elif input_user == '2':
-        pass
+        organize_files(os.getlogin())
+        main()
 
     else:
         print("Invalid value, please try again")
