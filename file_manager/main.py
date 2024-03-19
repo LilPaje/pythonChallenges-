a26ac5381596e09283b7951a4563329c14b5ac6f
LilPaje <brunovinicius03@hotmail.com>
2024-03-19 15:48:25 -0300

    Finally done de v1 of the code, implemented a way for creating folders to organize the files :) i'm a lil tired and the code is a lil bit crazy but it works

--- file_manager/main.py
+++ file_manager/main.py
@@ -33,6 +33,8 @@ def remove_files(user):
         for i in dir_file_list:
             print(i, end=", ")
 
+        
+
         print("\nThe files above will be permanently excluded. Do you want to proceed?Y/n")
         input_user = str(input("->"))
         if input_user == 'Y' or 'y':
@@ -71,7 +73,22 @@ def choose_folder():
         print("Invalid value, please try again")
         choose_folder()
 
-def create_folder():
+def create_folder(path):
+    print("This type of file don't have a folder in your system, do you want to create one?Y/n")
+    create_a_folder = input(str("-> "))
+    if create_a_folder == "Y" or create_a_folder == "y":
+        os.mkdir(path)
+        return True
+
+    elif create_a_folder == "N" or create_a_folder == "n":
+        return False
+
+    else: 
+        print("Invalid entry, please try again.")
+        time.sleep(2.5)
+        create_folder()
+
+    
         
 
 def organize_files(user):
@@ -79,22 +96,31 @@ def organize_files(user):
     
 
     input_user = choose_folder()
-    path = f'C:/Users/{user}/{input_user}'
-    list_itens_on_folder = os.listdir(path)
-    list_itens_destiny_folder = os.listdir(f'C:/Users/{user}/Documents/Organizing Files/teste')
     if input_user == "-1":
         pass
     else: 
+        path = f'C:/Users/{user}/{input_user}'
+        list_itens_on_folder = os.listdir(path)
+        if "Organizing Files" not in os.listdir(f'C:/Users/{user}/Documents/'):
+            os.mkdir(f'C:/Users/{user}/Documents/Organizing Files/')
+        list_itens_destiny_folder = os.listdir(f'C:/Users/{user}/Documents/Organizing Files/')
         print("Organizing")
         for j in list_itens_on_folder:
             if j[-3:] in list_itens_destiny_folder:
                 shutil.move(path+'/'+j,f'C:/Users/{user}/Documents/Organizing Files/'+j[-3:])
                 #print(j[-4:])
+                print(list_itens_destiny_folder)
             else:
-                print("This type of file do")
-                shutil.move(path+'/'+j,f'C:/Users/{user}/Documents/Organizing Files/other')
+                new_folder = create_folder(f'C:/Users/{user}/Documents/Organizing Files/'+j[-3:])
+                if new_folder == True:
+                    shutil.move(path+'/'+j,f'C:/Users/{user}/Documents/Organizing Files/'+j[-3:])
+                    list_itens_destiny_folder.append(j[-3:])
+                else: 
+                    if "other" not in os.listdir(f'C:/Users/{user}/Documents/Organizing Files'):
+                        os.mkdir(f'C:/Users/{user}/Documents/Organizing Files/other')
+        
+                    shutil.move(path+'/'+j,f'C:/Users/{user}/Documents/Organizing Files/other/')
             
-    
 
 def main():
     welcome() 
